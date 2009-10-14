d170224735282e20a99abbf0ce0ff17057e28286
Jacob Atzen <jacob@jacobatzen.dk>
2009-10-14 21:06:18 +0200

    Added next spec

--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -3,4 +3,10 @@ describe Stack do
   it "should be empty when first created" do
     Stack.new.should be_empty
   end
+  
+  it "should not be empty when something is pushed onto it" do
+    stack = Stack.new
+    stack.push(1)
+    stack.should_not be_empty
+  end
 end
